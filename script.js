d1d1222c1b82e962172a7c87981654c9144cf92c
Vasil <vasil.todorov@student.fontys.nl>
2022-10-22 15:52:02 +0200

    set up JS skeleton

--- script.js
+++ script.js
@@ -1,4 +1,44 @@
-const canvas = document.getElementById("canvas1");
-const ctx = canvas.getContext("2d");
-canvas.width = 500;
-canvas.height = 800;
+window.addEventListener('load', function(){
+    const canvas = document.getElementById("canvas1");
+    const ctx = canvas.getContext("2d");
+    canvas.width = 500;
+    canvas.height = 800;
+
+    class Game {
+      constructor() {
+        this.ememies = [];
+      }
+      update() {
+
+      }
+      draw() {
+
+      }
+      #addNewEnemy() {
+
+      }
+    }
+
+    class Enemy {
+      constructor() {
+
+      }
+      update() {
+
+      }
+      draw() {
+
+      }
+    }
+
+    let lastTime = 1;
+    function animate(timeStamp) {
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      const deltaTime = timeStamp - lastTime;
+      lastTime = timeStamp;
+      console.log(deltaTime)
+      //some code
+      requestAnimationFrame(animate);
+    }
+    animate(0)
+})
