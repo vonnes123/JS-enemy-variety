50232d5524cffabfc3566c99d861f518bb2db102
Vasil <vasil.todorov@student.fontys.nl>
2022-10-23 09:09:22 +0200

    set up worm class

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ window.addEventListener('load', function(){
             this.width = width;
             this.height = height;
             this.enemies = [];
-            this.enemyInterval = 1000;
+            this.enemyInterval = 100;
             this.enemyTimer = 0;
         }
         update(deltaTime) {
@@ -21,33 +21,44 @@ window.addEventListener('load', function(){
           } else {
             this.enemyTimer+= deltaTime;
           }
-          this.enemies.forEach(objeckt => objeckt.update());
+          this.enemies.forEach(objeckt => objeckt.update(deltaTime));
         }
         draw() {
-          this.enemies.forEach((objeckt) => objeckt.draw());
+          this.enemies.forEach((objeckt) => objeckt.draw(this.ctx));
         }
         #addNewEnemy() {
-            this.enemies.push(new Enemy(this));
+            this.enemies.push(new Worm(this));
+            this.enemies.sort(function(a,b){
+              return a.y - b.y;
+            })
         }
     }
 
     class Enemy {
       constructor(game) {
         this.game = game;
-        this.x = this.game.width;
-        this.y = Math.random() * this.game.height;
-        this.width = 100;
-        this.height = 100;
         this.markedForDeletion = false;
       }
-      update() {
-        this.x--;
-
-        //remove enemies
+      update(deltaTime) {
+        this.x-= this.vx;
         if (this.x < 0 - this.width) this.markedForDeletion = true;
       }
       draw(ctx) {
-        ctx.fillRect(this.x, this.y, this.width, this.height)
+        ctx.drawImage(this.image, 0, 0, this.spriteWidth, this.spriteHeight, this.x , this.y, this.width, this.height);
+      }
+    }
+
+    class Worm extends Enemy {
+      constructor(game){
+        super(game);
+        this.spriteWidth = 229;
+        this.spriteHeight = 171;
+        this.width = this.spriteWidth/2;
+        this.height = this.spriteHeight/2;
+        this.x = this.game.width;
+        this.y = Math.random() * this.game.height;
+        this.image = worm;
+        this.vx = Math.random() * 0.1 + 5;
       }
     }
 
